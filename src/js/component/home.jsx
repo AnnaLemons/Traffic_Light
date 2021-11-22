97b2da6e1c88817ae7ce85028bbd70c6b6fe29a8
AnnaLemons <a.lopezdelg@gmail.com>
2021-11-22 09:25:42 +0000

    saved

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import Bulb from "./bulb.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [color, setColor] = useState({
+		stop: "empty",
+		ready: "empty",
+		run: "empty"
+	});
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div className="pole-bar"></div>
+			<div className="container">
+				<Bulb
+					light={color.stop}
+					switchLight={() => {
+						setColor({
+							stop: "red",
+							ready: "empty",
+							run: "empty"
+						});
+					}}
+				/>
+				<Bulb
+					light={color.ready}
+					switchLight={() => {
+						setColor({
+							stop: "empty",
+							ready: "yellow",
+							run: "empty"
+						});
+					}}
+				/>
+				<Bulb
+					light={color.run}
+					switchLight={() => {
+						setColor({
+							stop: "empty",
+							ready: "empty",
+							run: "green"
+						});
+					}}
+				/>
+			</div>
 		</div>
 	);
 };
